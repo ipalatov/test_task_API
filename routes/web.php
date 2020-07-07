fa1146e557e445af1a5027967e0a064a12daf030
Ivan <ivan.s.palatov@gmail.com>
2020-07-07 10:07:33 +0400

    cleaning files

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,3 @@ use Illuminate\Support\Facades\Route;
   | contains the "web" middleware group. Now create something great!
   |
  */
-
-Route::get('/', ['as' => 'home', 'uses'=> 'admin\Core@showWelcome']);
-//Route::get('/', ['uses'=>'api\v1\ClientsController@getClients']);
-
-Route::match(['get', 'post'],'/test_page/{id?}', ['as' => 'test_page', 'uses'=> 'admin\Core@showTest_page']);
-
-Route::get('/about', ['as'=> 'about', 'uses'=> 'admin\AboutController@showAbout']);
-
-
-
-
-Route::get('/articles', ['uses' => 'admin\Core@getArticles', 'as' => 'articles']);
-Route::get('/article/{page}', ['middleware'=> 'mymiddle:admin1','uses' => 'admin\Core@getArticle', 'as' => 'article']);
-
-
-// list pages
-Route::resource('/pages', 'admin\CoreResource', ['only' => ['index', 'show']]);
-// only - только нужные методы(маршруты), except - исключения. Всего 7 методов-маршрутов.
